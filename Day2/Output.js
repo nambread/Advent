4570debec7177c877115de1df2143774f73a276d
Michael Nam <nambread@users.noreply.github.com>
2015-12-02 22:13:57 +0000

    Solution to Problem 2-1 Equation to calculate the surface area was wrong. Starting on 2-2.

--- Day2/Output.js
+++ Day2/Output.js
@@ -8,24 +8,29 @@ var DeepThought = function (input) {
     this.input = input;
     this.index = 0;
     this.getPresentReg = /(([0-9]+)x([0-9]+)x([0-9]+))/g
-    this.countPresents = this.input.split("\n");
-    this.countMatches = 0;
     this.lotsOfPaper = 0;
+    this.lotsOfRibbon = 0;
 }
 
 DeepThought.prototype.getNextPresent = function (){
     var dimensions;
     var match = this.getPresentReg.exec(this.input);
-    dimensions = !(match) ? null : [parseInt(match[1], 10), parseInt(match[2], 10), parseInt(match[3], 10)];
+    dimensions = !(match) ? null : [parseInt(match[2], 10), parseInt(match[3], 10), parseInt(match[4], 10)];
     //console.log(dimensions)
     return dimensions
 }
 
 DeepThought.prototype.calculateAreaNeeded = function (dimensions) {
-    var paper = 2 * dimensions[0] + 2 * dimensions[1] + 2 * dimensions[2];
+    var paper = 2 * dimensions[0] * dimensions[1] + 2 * dimensions[1] * dimensions[2] + 2 * dimensions[2] * dimensions[0];
+    console.log(dimensions)
     dimensions.splice(dimensions.lastIndexOf(Math.max(dimensions[0], dimensions[1], dimensions[2])), 1);
     var slack = dimensions[0] * dimensions[1];
     return paper+slack
+}
+
+DeepThought.prototype.calculateRibbonNeeded = function (dimensions) {
+    console.log(dimensions);
+    return 0
 }
 
 DeepThought.prototype.theQuestion = function (){
@@ -38,17 +43,12 @@ window.onload = function () {
     var input = document.getElementById("input").innerText;
     //Calculate solution to the first problem:
     var output1 = document.createElement("p")
-    var answer1 = (function (input) {
+    var answers = (function (input) {
         var dp = new DeepThought(input);
         while (present = dp.getNextPresent()) {
             dp.lotsOfPaper += dp.calculateAreaNeeded(present);
-            dp.countMatches++;
         }
-        //verifying elements aren't missed
-        console.log(dp.countPresents);
-        console.log(dp.countPresents.length);
-        console.log(dp.countMatches);
-        console.log(dp.lotsOfPaper)
+        return [dp.lotsOfPaper, dp.lotsOfRibbon];
     })(input);
     //Output answer
     document.body.appendChild(output1);
