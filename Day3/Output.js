7139034b3a9aa5b8d3e2aa2c70aa43ab04e85a5f
Michael Nam <nambread@users.noreply.github.com>
2015-12-03 22:05:58 +0000

    I got through all of this before I realised Elements in an array can't have an index < 0. Will need to work out a different data structure.

--- Day3/Output.js
+++ Day3/Output.js
@@ -3,62 +3,72 @@
 
 
 //A solution to 3-1.
-var House = function (neighbourhood) {
-    this.x = 0;
-    this.y = 0;
-    this.north;
-    this.south;
-    this.east;
-    this.west;
+var House = function (x, y) {
+    this.x = x;
+    this.y = y;
     this.presents = 0;
-    this.neighbourhood = neighbourhood;
+    this.isHouse = true;
 }
 
 var Neighbourhood = function () {
-    this.houses = [];
+    this.housesGrid = [[]];
     this.xStreets = 0;
     this.yStreets = 0;
-    this.currentHouse = new House(this);
+    this.currentHouse = new House(0, 0);
+    this.housesGrid[0].push(this.currentHouse);
 }
 
-Neighbourhood.prototype.goToHouse = function (direction) {
-    
+Neighbourhood.prototype.goDirection = function (direction, buildHouses) {
+    var nextHouse = this.housesGrid[this.currentHouse.x+direction.x][this.currentHouse.y+direction.y];
+    if (!(nextHouse) && buildHouses) {
+        nextHouse = this.buildHouse({x:(this.currentHouse.x+direction.x),y:(this.currentHouse.y+direction.y)})
+    } else {
+        nextHouse = this.housesGrid[this.currentHouse.x + direction.x][this.currentHouse.y + direction.y]
+    }
+    this.currentHouse = nextHouse;
+    return nextHouse;
 }
 
-Neighbourhood.prototype.findHouse = function () {
+Neighbourhood.prototype.buildHouse = function (coords) {
+    var newHouse = new House(coords.x, coords.y);
+    this.updateGrid(newHouse,coords);
+    return newHouse
+}
 
+Neighbourhood.prototype.updateGrid = function (house, coords) {
+    if (!this.housesGrid[coords.x]) {
+        while (this.housesGrid.length < coords.x) {
+            this.housesGrid.push([]);
+        }
+    }
+    if (!this.housesGrid[coords.x][coords.y]) {
+        while (this.housesGrid[coords.x].length < coords.y - 1) {
+            this.housesGrid[coords.x].push([]);
+        }
+    }
+    this.housesGrid[coords.x][coords.y].push(house);
 }
 
 var Santa = function () {
     this.neighbourhood = new Neighbourhood();
-    this.preparedDirections = false;
     this.legend = {
-        "^": "north",
-        "v": "south",
-        ">": "east",
-        "<": "west"
+        "^": {orientation:"north", x: 0, y: 1},
+        "v": {orientation:"south", x: 0, y: -1},
+        ">": {orientation:"east", x: 1, y: 0},
+        "<": {orientation:"west", x: -1, y: 0}
     }
 }
 
-Santa.prototype.prepareDirections = function (input) {
-    var e = document.createElement('div');
-    e.innerHTML = input;
-    return e.childNodes.length === 0 ? null : e.childNodes[0].nodeValue;
-}
-
 Santa.prototype.readDirections = function (input, index) {
-    return input[index];
+    return this.legend[input[index]];
 }
 
 Santa.prototype.startDelivery = function (input) {
     var index = 0;
     while (direction = this.readDirections(input, index++)) {
-        this.deliverPresent(direction);
+        var nextHouse = this.neighbourhood.goDirection(direction, true);
+        nextHouse.presents++;
     }
-}
-
-Santa.prototype.deliverPresent = function (direction) {
-    console.log(this.legend[direction]);
 }
 
 //Display the solutions:
