ac46f6d1eba5d2da262a5ccd10b2f1ac753c668e
Michael Nam <nambread@users.noreply.github.com>
2015-12-03 20:28:54 +0000

    Santa reaches a neighbourhood, finds a house: Part 1 Santa understands his directions. Success!

--- Day3/Output.js
+++ Day3/Output.js
@@ -3,11 +3,68 @@
 
 
 //A solution to 3-1.
+var House = function (neighbourhood) {
+    this.x = 0;
+    this.y = 0;
+    this.north;
+    this.south;
+    this.east;
+    this.west;
+    this.presents = 0;
+    this.neighbourhood = neighbourhood;
+}
 
+var Neighbourhood = function () {
+    this.houses = [];
+    this.xStreets = 0;
+    this.yStreets = 0;
+    this.currentHouse = new House(this);
+}
 
+Neighbourhood.prototype.goToHouse = function (direction) {
+    
+}
+
+Neighbourhood.prototype.findHouse = function () {
+
+}
+
+var Santa = function () {
+    this.neighbourhood = new Neighbourhood();
+    this.preparedDirections = false;
+    this.legend = {
+        "^": "north",
+        "v": "south",
+        ">": "east",
+        "<": "west"
+    }
+}
+
+Santa.prototype.prepareDirections = function (input) {
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? null : e.childNodes[0].nodeValue;
+}
+
+Santa.prototype.readDirections = function (input, index) {
+    return input[index];
+}
+
+Santa.prototype.startDelivery = function (input) {
+    var index = 0;
+    while (direction = this.readDirections(input, index++)) {
+        this.deliverPresent(direction);
+    }
+}
+
+Santa.prototype.deliverPresent = function (direction) {
+    console.log(this.legend[direction]);
+}
 
 //Display the solutions:
 window.onload = function () {
-    //ermmm....
+    var input = document.getElementById("input").innerText;
+    var santa = new Santa();
+    santa.startDelivery(input);
 }
  
