5ecbecea7d945f7e5e041d1d083bb77ec03dac18
Michael Nam <nambread@users.noreply.github.com>
2015-12-03 19:06:21 +0000

    Start of Day 3 We'll see how this goes.

---
+++ Day3/Output.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+//Advent of code solution for Day 3 by Nambread.
+
+
+//A solution to 3-1.
+
+
+
+//Display the solutions:
+window.onload = function () {
+    //ermmm....
+}
